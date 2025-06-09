b62b6c6fe30889c291c1cd2f0e8f9ecf0d3eb899
Ibrahim <ibrahimmiishaq@gmail.com>
2025-06-09 14:08:51 -0700

    Completed SimpleTextureEffect Files for Clean-up

--- Framework/Graphics/Inc/SimpleTextureEffect.h
+++ Framework/Graphics/Inc/SimpleTextureEffect.h
@@ -4,25 +4,40 @@
 #include "Sampler.h"
 #include "VertexShader.h"
 #include "PixelShader.h"
+#include "MeshBuffer.h"
+#include "TextureManager.h"
 
 namespace IExeEngine::Graphics
 {
     class Camera;
-    class MeshBuffer;
 
     class SimpleTextureEffect
     {
     public:
+
+        struct RenderData
+        {
+            MeshBuffer mesh;
+            Math::Matrix4 matWorld;
+            TextureId textureId;
+        };
+
         void Initialize();
         void Terminate();
 
         void Begin();
         void End();
 
-        void Render(const MeshBuffer& mesh, const Math::Matrix4& matWorld);
+        void Render(const RenderData& renderData);
 
         void SetCamera(const Camera& camera);
 
     private:
+        ConstantBuffer mTransformBuffer;
+        VertexShader mVertexShader;
+        PixelShader mPixelShader;
+        Sampler mSampler;
 
+        const Camera* mCamera = nullptr;
+    };
 }
