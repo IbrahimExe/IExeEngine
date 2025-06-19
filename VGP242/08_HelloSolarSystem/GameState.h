14734e01b7da1f35d1709629378c5fe4636de6e6
Ibrahim <ibrahimmiishaq@gmail.com>
2025-06-18 21:08:04 -0700

    Updated GameState.h for Better cpp File Organisation

--- VGP242/08_HelloSolarSystem/GameState.h
+++ VGP242/08_HelloSolarSystem/GameState.h
@@ -16,29 +16,60 @@ public:
 	void DebugUI() override;
 
 private:
+	struct Planet
+	{
+		float radius;
+		IExeEngine::Math::Matrix4 centerObject;
+		float distanceFromCenter;
+		float orbitTranslation;
+		float rotationOnAxis;
+		float orbitAngle = 0.0f;
+		float selfRotationAngle = 0.0f;
+		IExeEngine::Graphics::SimpleTextureEffect::RenderData renderData;
+	};
+
+	struct Asteroid
+	{
+        float size;
+        IExeEngine::Math::Matrix4 centerObject;
+        float distanceFromCenter;
+        float orbitTranslation;
+        float selfRotationAngle = 0.0f;
+        IExeEngine::Graphics::SimpleTextureEffect::RenderData renderData;
+	};
+
 	void UpdateCamera(float deltaTime);
+
+    // Functions for Animating Planets & Moon
+	void UpdateRenderTargetCamera(float deltaTime, Math::Vector3 targetPosition, float objectRadius);
+	void UpdatePlanet(Planet& planet, float deltaTime);
+	void UpdateMoon(Planet& planet, const Planet& earth, float deltaTime);
 
 	IExeEngine::Graphics::Camera mCamera;
 	IExeEngine::Graphics::Camera mRenderTargetCamera;
 
-	// GPU Communication
+	// GPU communication
 	IExeEngine::Graphics::SimpleTextureEffect mSimpleTextureEffect;
 
-	// Render Object
+	// Render Objects
 	IExeEngine::Graphics::SimpleTextureEffect::RenderData mSpace;
-
 	IExeEngine::Graphics::SimpleTextureEffect::RenderData mSun;
-	IExeEngine::Graphics::SimpleTextureEffect::RenderData mMercury;
-    IExeEngine::Graphics::SimpleTextureEffect::RenderData mVenus;
-	IExeEngine::Graphics::SimpleTextureEffect::RenderData mEarth;
-    IExeEngine::Graphics::SimpleTextureEffect::RenderData mMars;
-    IExeEngine::Graphics::SimpleTextureEffect::RenderData mJupiter;
-    IExeEngine::Graphics::SimpleTextureEffect::RenderData mSaturn;
-    IExeEngine::Graphics::SimpleTextureEffect::RenderData mUranus;
-    IExeEngine::Graphics::SimpleTextureEffect::RenderData mNeptune;
 
-	IExeEngine::Graphics::SimpleTextureEffect::RenderData mAsteroid;
+	// Render Planets & Moon 
+	Planet mMercury;
+	Planet mVenus;
+	Planet mEarth;
+	Planet mMoon;
+	Planet mMars;
+	Planet mJupiter;
+	Planet mSaturn;
+	Planet mUranus;
+	Planet mNeptune;
+	Planet mPluto;
 
 	// Render Target
 	IExeEngine::Graphics::RenderTarget mRenderTarget;
+
+	// Render ImGui Orbit Rings
+	bool orbitRings = true;
 };
