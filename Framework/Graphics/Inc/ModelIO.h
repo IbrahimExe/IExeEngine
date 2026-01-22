3ec1a25a8512c762edac49943f61f026e8842ea9
Ibrahim <ibrahimmiishaq@gmail.com>
2026-01-21 18:40:24 -0800

    Add skeleton save/load functions to ModelIO
    Introduced SaveSkeleton and LoadSkeleton methods in ModelIO for serializing and deserializing skeleton data associated with models. SaveSkeleton implementation writes bone hierarchy and transforms to a file, while LoadSkeleton is stubbed for future implementation.

--- Framework/Graphics/Inc/ModelIO.h
+++ Framework/Graphics/Inc/ModelIO.h
@@ -12,5 +12,8 @@ namespace IExeEngine::Graphics
 
         void SaveMaterial(std::filesystem::path filePath, const Model& model);
         void LoadMaterial(std::filesystem::path filePath, Model& model);
+
+        void SaveSkeleton(std::filesystem::path filePath, Model& model);
+        void LoadSkeleton(std::filesystem::path filePath, Model& model);
     }
 }
