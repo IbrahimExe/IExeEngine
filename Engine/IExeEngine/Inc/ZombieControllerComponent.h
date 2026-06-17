fbf095b0d28c5cdcad6297de92c04475c239ac21
Ibrahim <ibrahimmiishaq@gmail.com>
2026-06-16 23:02:52 -0700

    Using Player Controller as Template

--- Engine/IExeEngine/Inc/ZombieControllerComponent.h
+++ Engine/IExeEngine/Inc/ZombieControllerComponent.h
@@ -4,9 +4,31 @@
 
 namespace IExeEngine
 {
+    class TransformComponent;
+    class RigidBodyComponent;
+
     class ZombieControllerComponent : public Component
     {
     public:
         SET_TYPE_ID(ComponentId::ZombieController);
+
+        void Initialize() override;
+
+        void Terminate() override;
+
+        void Update(float deltaTime) override;
+
+        void DebugUI() override;
+
+        void Deserialize(const rapidjson::Value& value) override;
+
+    private:
+        TransformComponent* mTransformComponent = nullptr;
+        RigidBodyComponent* mRigidBodyComponent = nullptr;
+
+        float mShiftSpeed = 10.0f;
+        float mMoveSpeed = 2.0f;
+        float mTurnSpeed = 0.1f;
+        float mJumpSpeed = 20.0f;
     };
 }
